2e865067e8add6fdc3c5f2f55c93529cbb03869c
Aryan Vikash <aryanvikash@pop-os.localdomain>
2021-02-21 11:12:12 +0530

    [modified]: seo enhancement & SSR

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-
+import Head from "next/head";
 
 import Hero from "../components/hero"
 import Languages from "../components/languages"
@@ -6,12 +6,33 @@ import Footer from "../components/footer"
 
 export default function Home() {
 
-
+const description = "A developer with few Skills"
+const pageTitle = "Aryan Vikash"
+const siteName = "aryanvikash"
 
 
 
   return (
     <div>
+      <Head>
+      <title>{pageTitle}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="A developer with few Skills" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
++       <meta property="og:description" content={description} key="ogdesc" />
+        <meta name="twitter:card" content={description} key="twcard" />
+        <meta name="twitter:creator" content="@_aryanvikash" key="twhandle" />
+
+        {/* Open Graph */}
+        <meta property="og:url" content="https://aryanvikash.com" key="ogurl" />
+        <meta property="og:image" content="https://github.com/aryanvikash.png" key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+                
+      </Head>
+
       <Hero />
       <Languages />
       <Footer />
--- pages/projects.js
+++ pages/projects.js
@@ -4,28 +4,39 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import config from "../config"
 
-export default function projects() {
-      const uri = `https://api.github.com/users/${config.githubUsername}/repos`;
-      const [data, setData] = useState({});
+export default function projects({projects}) {
+  // const uri = `https://api.github.com/users/${config.githubUsername}/repos`;
+  //     const [data, setData] = useState({});
 
-      useEffect(() => {
-        axios
-          .get(uri)
-          .then((res) => {
-            setData(res.data);
-          })
-          .catch((e) => {
-            console.log(e.message);
-          });
-      }, [uri]);
+      // useEffect(() => {
+      //   axios
+      //     .get(uri)
+      //     .then((res) => {
+      //       setData(res.data);
+      //     })
+      //     .catch((e) => {
+      //       console.log(e.message);
+      //     });
+      // }, [uri]);
 
 
 
     return(
 
     <div>
-        <Projects data={data} />
+        <Projects data={projects} />
     </div>
 
     );
+}
+
+export async function getStaticProps(context) {
+  const uri = `https://api.github.com/users/${config.githubUsername}/repos`;
+ 
+    const res = await axios.get(uri)
+ 
+
+  return {
+    props: {projects:res.data}, // will be passed to the page component as props
+  }
 }
