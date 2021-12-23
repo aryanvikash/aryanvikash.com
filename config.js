4fe857e61aa208d7958fb61b2416b96f1caa68d1
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-12-23 18:14:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ export default {
   websiteName: "@Jigarvarma2005",
   heroHead: "Hey There !!",
   heroText: "Jigar Varma is here , I’m ",
+  email: "contact@jigarvarma.tech",
   facebookUsername: "jigarvarma2005",
   instaUsername: "jigarvarma2005",
   typedArray : ["Bad Boy ....","College Student ....","Noob Developer ....","Open-source Lover ....","Learning C++ ....","Indian🇮🇳❤️ ....","Noob Programmer ....","Self Learner ....","Learning Bash ....","Learning python3 ...."]
