100ea53eeb9fd63d3343700ff996a4cc1005d577
milliorn <scottmilliorn@gmail.com>
2023-02-10 11:44:16 -0800

    fixed html does not having a lang attribute

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// If your website supports multiple languages, you can even put various values in the locales array.
+module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
